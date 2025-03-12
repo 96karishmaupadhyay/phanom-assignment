fd5d095db8eceedc2d1c4974c4d9963661abfdcb
96karishmaupadhyay <karishmaupadhyay1996@gmail.com>
2025-03-12 14:15:04 +0530

    explore updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import logo from "../../assets/logo.png"
 import styles from "./navbar.module.css"
 import frame1 from "../../assets/frame1.png"
+import menu from "../../assets/menu.png"
 const Navbar = () => {
   return (
     <>
+    
+    
       <nav className={styles.nav}>
         <div>
           <img src={logo} alt="logo" height={40} width={180}/>
+          <img
+            src={menu}
+            alt="menu"
+            className={styles.menuIcon}
+            onClick={() => setIsOpen(!isOpen)}
+          />
         </div>
         <div>
           <ul className={styles.list}>
